1226205e18bc5f858e21f4e221589f0541d678b9
Dutkercz <dutkercz@gmail.com>
2025-03-17 16:14:49 -0300

    Start project

---
+++ src/main/java/com/PrismaMobi/Prisma_Mobi/PrismaMobiApplication.java
@@ -0,0 +1,13 @@
+package com.PrismaMobi.Prisma_Mobi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrismaMobiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrismaMobiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PrismaMobi/Prisma_Mobi/PrismaMobiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PrismaMobi.Prisma_Mobi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrismaMobiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
