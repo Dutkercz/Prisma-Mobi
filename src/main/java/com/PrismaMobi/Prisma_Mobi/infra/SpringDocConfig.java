1d261911409eb297a79b4485c44b30bf4856e9ce
Dutkercz <dutkercz@gmail.com>
2025-05-08 01:25:05 -0300

    Forçar o uso de HTTPS com o springdoc

--- src/main/java/com/PrismaMobi/Prisma_Mobi/infra/SpringDocConfig.java
+++ src/main/java/com/PrismaMobi/Prisma_Mobi/infra/SpringDocConfig.java
@@ -3,15 +3,20 @@ package com.PrismaMobi.Prisma_Mobi.infra;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SpringDocConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
-        return new OpenAPI().components(new Components().
+        return new OpenAPI()
+                .servers(List.of(new Server().url("https://prisma-mobi-production.up.railway.app")))
+                .components(new Components().
                 addSecuritySchemes("bearer-key",
                         new SecurityScheme().type(
                                 SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
