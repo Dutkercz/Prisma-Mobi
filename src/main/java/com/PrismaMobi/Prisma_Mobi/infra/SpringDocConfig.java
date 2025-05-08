66fec0036e320cff7d8eb7e219019b4c4c1a530d
Dutkercz <dutkercz@gmail.com>
2025-05-08 02:00:15 -0300

    Adição de informações na documentação

--- src/main/java/com/PrismaMobi/Prisma_Mobi/infra/SpringDocConfig.java
+++ src/main/java/com/PrismaMobi/Prisma_Mobi/infra/SpringDocConfig.java
@@ -2,6 +2,8 @@ package com.PrismaMobi.Prisma_Mobi.infra;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
@@ -19,6 +21,13 @@ public class SpringDocConfig {
                 .components(new Components().
                 addSecuritySchemes("bearer-key",
                         new SecurityScheme().type(
-                                SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
+                                SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
+                .info(new Info()
+                        .title("Prisma-Mobi API")
+                        .description("Documentação da API para o sistema PRISMA-MOBI")
+                        .contact(new Contact()
+                                .name("Cristian Tiago Dutkercz Rosa")
+                                .email("dutkercz@gmail.com")
+                                .url("https://github.com/Dutkercz")));
     }
 }
